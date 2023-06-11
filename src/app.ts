01d7bff9ca58691b0da1da1b95779f6643effb19
Justin Sacks <sacksjust@gmail.com>
2023-06-10 22:49:14 -0400

    Refactored routes to follow a more logical naming convention, using /logs as the main route

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,43 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import { listDirectoryContents } from "./routes/list";
-import { getFile } from "./routes/file";
-
+import { getLogFile, getLogFiles } from "./routes/logs";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get("/list", (req: Request, res: Response) => {
+const root = process.platform === "win32" ? "C:/temp" : "/var/log";
+
+app.get("/logs", (req: Request, res: Response) => {
     try {
-        listDirectoryContents(req, res);
-    }
-    catch (e) {
+        getLogFiles(req, res, root);
+    } catch (e) {
         res.statusCode = 500;
         res.send(e);
     }
 });
 
-app.get("/file", (req: Request, res: Response) => {
+app.get("/logs/:fileName", (req: Request, res: Response) => {
     try {
-        getFile(req, res);
+        const fileName = req.params.fileName;
+        getLogFile(req, res, root, fileName);
+    } catch (e) {
+        res.statusCode = 500;
+        res.send(e);
     }
-    catch (e) {
+});
+
+app.get("/log", (req: Request, res: Response) => {
+    try {
+        const fileName = req.params.fileName;
+        getLogFile(req, res, root, fileName);
+    } catch (e) {
         res.statusCode = 500;
         res.send(e);
     }
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`Logget listening on port ${port} serving files from ${root}`);
 });
---
+++ src/routes/logs.ts
@@ -0,0 +1,69 @@
+import { Request, Response } from "express";
+import fs from "fs";
+import path from "path";
+import { Dir, DirectoryContents } from "../models/DirectoryContents";
+
+export const getLogFiles = (req: Request, res: Response, root: string) => {
+    if (!fs.existsSync(root)) {
+        res.statusCode = 400;
+        res.send(`Invalid path '${root}'`);
+    } else {
+        const dirents: fs.Dirent[] = fs.readdirSync(root, {
+            withFileTypes: true,
+        });
+        const files = [];
+        const dirs = [];
+        for (var i = 0; i < dirents.length; i++) {
+            const dirent = dirents[i];
+            if (dirent.isDirectory()) {
+                const dir: Dir = {
+                    name: dirent.name,
+                };
+                dirs.push(dir);
+            } else {
+                const fullPath = path.join(root, dirent.name);
+                const stats = fs.statSync(fullPath);
+                const file = {
+                    name: dirent.name,
+                    size: stats.size,
+                };
+                files.push(file);
+            }
+        }
+        const dirContents: DirectoryContents = {
+            path: root,
+            files: files,
+            dirs: dirs,
+        };
+        res.send(dirContents);
+    }
+};
+
+export const getLogFile = (
+    req: Request,
+    res: Response,
+    root: string,
+    fileName: string
+) => {
+    const fullPath = path.join(root, fileName);
+
+    // TODO consider returning the same error for both these cases for obscurity
+    if (!fs.existsSync(fullPath)) {
+        res.statusCode = 400;
+        res.send(`File does not exist: '${fullPath}`);
+    } else {
+        const stats = fs.statSync(fullPath);
+        if (stats.isDirectory()) {
+            res.statusCode = 400;
+            res.send(`Path must point to a file: '${fullPath}`);
+        } else {
+            res.writeHead(200, {
+                "Content-Type": "application/octet-stream",
+                "Content-Disposition": `attachment; ${fileName}`,
+                "Content-Length": stats.size,
+            });
+            const rs = fs.createReadStream(fullPath);
+            rs.pipe(res);
+        }
+    }
+};
